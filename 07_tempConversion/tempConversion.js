8f76908af77817a25a76f88bd43eec3fbbd57abc
Mathias <72262110+Mathias9961@users.noreply.github.com>
2023-10-29 15:11:21 -0300

    Solve 7th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  /*
+  Formula to convert from F to C:
+  x °F ≘ (x − 32) × 5/9 °C
+  */
+
+  return parseFloat(((temperatureInFahrenheit - 32) * 5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  /*
+  Formula to convert from C to F:
+  x °C ≘ (x * 5/9) + 32 °F
+  */
+  return parseFloat(((temperatureInCelsius * 9/5) + 32) .toFixed(1));
 };
 
 // Do not edit below this line
