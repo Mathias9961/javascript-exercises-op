d16da1c2db688a5c1d30b41628a6f373d70426e9
Mathias <72262110+Mathias9961@users.noreply.github.com>
2023-10-29 11:42:32 -0300

    Solve 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(wordToReverse) {
+    let reversedWord = "";
 
+    for (let i = wordToReverse.length; i > 0; i--) {
+        reversedWord += wordToReverse[i-1];
+    }
+
+    return reversedWord;
 };
 
 // Do not edit below this line
