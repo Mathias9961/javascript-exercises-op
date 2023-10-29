e1c89cfb6f3a76b8a017d3b1eaccfe61c16b518f
Mathias <72262110+Mathias9961@users.noreply.github.com>
2023-10-29 14:42:41 -0300

    Solve 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
 
+    //Final result for the sum of all the numbers between
+    //the smaller number and the largest number (both included) 
+    let result = 0;
+
+    let fromNumber = firstNumber;
+    let toNumber = secondNumber;
+
+    //Confirm that both arguments are positive numbers
+    if (
+        typeof fromNumber !== 'number' ||
+        typeof toNumber !== 'number' ||
+        fromNumber < 0 ||
+        toNumber < 0
+        ) {
+        result = "ERROR";
+    } else {
+        //reorder the parameters if the second argument is smaller
+        if (firstNumber > secondNumber) {
+            fromNumber = secondNumber;
+            toNumber = firstNumber;
+        }
+
+        //for each number between the two arguments, add the value to the result
+        for (let i = fromNumber; i <= toNumber; i++) {
+            result += i;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
